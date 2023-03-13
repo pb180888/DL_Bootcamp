b8a2e18ff9a04b2c9c044f605e429082fe417a8f
Paul Bogdanov <pb180888@gmail.com>
2023-03-13 10:26:52 +0200

    tried ti solve one problem in js

--- Week1/day2/ExrciseXPGOLD-CreateALandingPage/script.js
+++ Week1/day2/ExrciseXPGOLD-CreateALandingPage/script.js
@@ -1,5 +1,6 @@
 const butSignUp = document.querySelector("#button");
 const butExit = document.querySelector("#exit");
+const element = document.querySelector(".register");
 
 let clickSignUp1 = butSignUp.addEventListener("click", function () {
   document.querySelector(".register").classList.remove("hidden");
@@ -8,3 +9,14 @@ let clickSignUp1 = butSignUp.addEventListener("click", function () {
 let clickExit = butExit.addEventListener("click", function () {
   document.querySelector(".register").classList.add("hidden");
 });
+
+// if (element) {
+//   butSignUp.addEventListener("click", function () {
+//     element.classList.remove("hidden");
+//   });
+// }
+// if (element.classList.remove("hidden")) {
+//   butSignUp.addEventListener("click", function () {
+//     element.classList.add("hidden");
+//   });
+// }
