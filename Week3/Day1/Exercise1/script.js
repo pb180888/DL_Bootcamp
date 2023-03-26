fbfd58672986b3ece8626b92568876db93009322
Paul Bogdanov <pb180888@gmail.com>
2023-03-26 17:04:55 +0300

    added Exercises - 1,2,3 of Day1 of Week3

---
+++ Week3/Day1/Exercise1/script.js
@@ -0,0 +1,18 @@
+// 1. Create these variables and give them values:
+// addressNumber
+// addressStreet
+// country
+
+const addressNumber = 1;
+const addressStreet = "Yosef";
+const country = "Israel";
+
+// 2. Write a variable named globalAddress, and concatenate inside, the variables:
+
+// addressNumber
+// addressStreet
+// country
+
+globalAddress =
+  "I live in " + addressStreet + " " + addressNumber + " in " + country;
+console.log(globalAddress);
---
+++ Week3/Day1/Exercise2/script.js
@@ -0,0 +1,16 @@
+// 1. Store your birth year in a variable.
+
+const birthYear = 1988;
+
+// 2. Store a future year in a variable.
+
+let futureYear = 2024;
+
+// 3. Calculate your possible ages for that year based on the stored values.
+
+myPossibleAge = futureYear - birthYear;
+console.log(myPossibleAge);
+
+// 4. Display : "I will be NN in YYYY", substituting the values.
+
+console.log(`I will be ${myPossibleAge} in ${futureYear}`);
