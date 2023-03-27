275264deec72897063b664f161ef232462f38699
Paul Bogdanov <pb180888@gmail.com>
2023-03-27 12:13:40 +0300

    added a little cahnges in code of ExerciseXPNinja of Exercise4_Boom

--- Week3/Day2/ExercisesXP/ExercisesXPNinja/Exercise4_Boom/script.js
+++ Week3/Day2/ExercisesXP/ExercisesXPNinja/Exercise4_Boom/script.js
@@ -7,65 +7,69 @@ let n = "";
 let newWord;
 let boomWord = "boom";
 
-// If the number given is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
-
-// If the number given is evenly divisible by 5, return the string in ALL CAPS.
-if (userNumber % 5 === 0) {
-  // If the number given is evenly divisible by 2, add a exclamation mark to the end.
-  if (userNumber % 2 === 0) {
-    // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
-    if (userNumber > 2) {
-      for (let i = 0; i < userNumber; i++) {
-        n = n + "o";
-      }
-      newWord = `b${n}m!`;
-      alert(newWord.toUpperCase());
-    }
-    // If the number given is less than 2 : return “boom”
-    else {
-      alert(`${boomWord.toUpperCase()}!`);
-    }
-  } else if (userNumber % 2 !== 0) {
-    // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
-    if (userNumber > 2) {
-      for (let i = 0; i < userNumber; i++) {
-        n = n + "o";
-      }
-      newWord = `b${n}m`;
-      alert(newWord.toUpperCase());
-    }
-    // If the number given is less than 2 : return “boom”
-    else {
-      alert(`${boomWord.toUpperCase()}`);
-    }
-  }
+if (userNumber === 2) {
+  alert("It's not bigger than 2 and it's not less than 2");
 } else {
-  // If the number given is evenly divisible by 2, add a exclamation mark to the end.
-  if (userNumber % 2 === 0) {
-    // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
-    if (userNumber > 2) {
-      for (let i = 0; i < userNumber; i++) {
-        n = n + "o";
+  // If the number given is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
+
+  // If the number given is evenly divisible by 5, return the string in ALL CAPS.
+  if (userNumber % 5 === 0) {
+    // If the number given is evenly divisible by 2, add a exclamation mark to the end.
+    if (userNumber % 2 === 0) {
+      // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+      if (userNumber > 2) {
+        for (let i = 0; i < userNumber; i++) {
+          n = n + "o";
+        }
+        newWord = `b${n}m!`;
+        alert(newWord.toUpperCase());
       }
-      newWord = `b${n}m!`;
-      alert(newWord);
-    }
-    // If the number given is less than 2 : return “boom”
-    else {
-      alert(`${boomWord}!`);
-    }
-  } else if (userNumber % 2 !== 0) {
-    // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
-    if (userNumber > 2) {
-      for (let i = 0; i < userNumber; i++) {
-        n = n + "o";
+      // If the number given is less than 2 : return “boom”
+      else if (userNumber < 2) {
+        alert(`${boomWord.toUpperCase()}!`);
       }
-      newWord = `b${n}m`;
-      alert(newWord);
+    } else if (userNumber % 2 !== 0) {
+      // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+      if (userNumber > 2) {
+        for (let i = 0; i < userNumber; i++) {
+          n = n + "o";
+        }
+        newWord = `b${n}m`;
+        alert(newWord.toUpperCase());
+      }
+      // If the number given is less than 2 : return “boom”
+      else if (userNumber < 2) {
+        alert(`${boomWord.toUpperCase()}`);
+      }
     }
-    // If the number given is less than 2 : return “boom”
-    else {
-      alert(`${boomWord}`);
+  } else {
+    // If the number given is evenly divisible by 2, add a exclamation mark to the end.
+    if (userNumber % 2 === 0) {
+      // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+      if (userNumber > 2) {
+        for (let i = 0; i < userNumber; i++) {
+          n = n + "o";
+        }
+        newWord = `b${n}m!`;
+        alert(newWord);
+      }
+      // If the number given is less than 2 : return “boom”
+      else if (userNumber < 2) {
+        alert(`${boomWord}!`);
+      }
+    } else if (userNumber % 2 !== 0) {
+      // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+      if (userNumber > 2) {
+        for (let i = 0; i < userNumber; i++) {
+          n = n + "o";
+        }
+        newWord = `b${n}m`;
+        alert(newWord);
+      }
+      // If the number given is less than 2 : return “boom”
+      else if (userNumber < 2) {
+        alert(`${boomWord}`);
+      }
     }
   }
 }
