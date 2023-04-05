04461e3fffb9d69b98196c50d115cb4f78eec6d4
Paul Bogdanov <pb180888@gmail.com>
2023-04-05 19:56:34 +0300

    added Daily Challenge: 99 Bottles Of Beer of Day4 of Week4

---
+++ Week4/Day4/DailyChallenge_99BottlesOfBeer/script.js
@@ -0,0 +1,65 @@
+// Have you heard the infamous song “99 Bottles of Beer?”
+// In this exercise you need to console.log the lyrics of our own 99 Bottles of Beer song.
+
+// ==============================
+// Example
+// ==============================
+
+// 99 bottles of beer on the wall
+// 99 bottles of beer
+// Take 1 down, pass it around
+// 98 bottles of beer on the wall
+
+// 98 bottles of beer on the wall
+// 98 bottles of beer
+// Take 2 down, pass them around
+// 96 bottles of beer on the wall
+
+// 96 bottles of beer on the wall
+// 96 bottles of beer
+// Take 3 down, pass them around
+// 93 bottles of beer on the wall
+
+// ect …
+
+function bottlesOfBeer() {
+  let numberOfBottles = 0;
+  let takeOneMoreTime = 1;
+  let word = "";
+  do {
+    numberOfBottles = Number(prompt("Enter the number of bottles"), 0);
+  } while (!Number(numberOfBottles));
+  let song = `${numberOfBottles} bottles of beer on the wall \
+${numberOfBottles} bottles of beer \
+Take ${takeOneMoreTime} down, pass ${word} around \
+${numberOfBottles - 1} bottles of beer on the wall`;
+  let minusBottles = 0;
+  let plusTakeOneMoreTime = 0;
+  for (let i = 0; i < numberOfBottles; i++) {
+    if (
+      numberOfBottles - minusBottles > 1 &&
+      numberOfBottles - minusBottles >= plusTakeOneMoreTime
+    ) {
+      if (plusTakeOneMoreTime < 1) {
+        word = "it";
+      } else {
+        word = "them";
+      }
+
+      minusBottles = minusBottles + i;
+      plusTakeOneMoreTime = plusTakeOneMoreTime + 1;
+      bigWord = numberOfBottles - minusBottles - plusTakeOneMoreTime;
+      if (bigWord <= 0) {
+        bigWord = 0;
+      }
+      song = `${numberOfBottles - minusBottles} bottles of beer on the wall \
+        ${numberOfBottles - minusBottles} bottles of beer \
+        Take ${plusTakeOneMoreTime} down, pass ${word} around \
+        ${bigWord} bottles of beer on the wall`;
+      console.log(song);
+    } else if (bigWord <= 0) {
+      break;
+    }
+  }
+}
+bottlesOfBeer();
