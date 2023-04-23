fec50f8a46c4904258722ec9cb5d4142688cc0fc
Paul Bogdanov <pb180888@gmail.com>
2023-04-23 18:54:35 +0300

    added Daily Challenge: Letters with mistakes

--- Week5/Day4/DailyChallenges/DailyChallenge_Letters/script.js
+++ Week5/Day4/DailyChallenges/DailyChallenge_Letters/script.js
@@ -7,28 +7,28 @@ form.appendChild(input);
 document.body.appendChild(form);
 
 // 2. Hint: use one of the following events to remove any character that is not a letter
-
+var hello = form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (input.value.length < 1) {
+    alert("Empty");
+  }
+  if (Number(input.value)) {
+    alert("Enter string");
+  } else {
+    for (let i = 0; i < input.value.length; i++) {
+      if (
+        !(input.value[i] < "A") ||
+        (!(input.value[i] > "Z") && !(input.value[i] < "a")) ||
+        !(input.value[i] > "z")
+      ) {
+        input.value[i].remove();
+      }
+    }
+  }
+});
 input.addEventListener("keydown", function (event) {
   if (event.code === "Enter") {
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      if (input.value.length < 1) {
-        alert("Empty");
-      }
-      if (Number(input.value)) {
-        alert("Enter string");
-      } else {
-        for (let i = 0; i < input.value.length; i++) {
-          if (
-            !(input.value[i] < "A") ||
-            (!(input.value[i] > "Z") && !(input.value[i] < "a")) ||
-            !(input.value[i] > "z")
-          ) {
-            input.value[i].remove();
-          }
-        }
-      }
-    });
+    hello;
   }
 });
 
