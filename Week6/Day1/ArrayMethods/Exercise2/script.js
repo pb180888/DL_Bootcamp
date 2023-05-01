c59cba3216700e7c2dc0ad434e92c7ecb8ac7e65
Paul Bogdanov <pb180888@gmail.com>
2023-05-01 23:00:02 +0300

    added whole exercises of ArrayMethods

--- Week6/Day1/ArrayMethods/Exercise2/script.js
+++ Week6/Day1/ArrayMethods/Exercise2/script.js
@@ -3,7 +3,7 @@ const words = ["wow", "hey", "bye"];
 // Check if at least one element of the array starts with the letter 'h'
 
 let result = words.some((index) => {
-  return index.slice(0, 1) === "h";
+  return index[0] === "h";
 });
 console.log(result);
 
--- Week6/Day1/ArrayMethods/Exercise3/script.js
+++ Week6/Day1/ArrayMethods/Exercise3/script.js
@@ -2,6 +2,6 @@ const words = ["hello", "hey", "hola"];
 
 // Check if all the elements of the array start with the letter 'h'
 let result = words.every((index) => {
-  return index.slice(0.1) === "h";
+  return index[0] == "h";
 });
 console.log(result);
