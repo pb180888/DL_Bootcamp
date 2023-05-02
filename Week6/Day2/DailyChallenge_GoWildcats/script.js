029ff95ec17726a7f888bab711e1b706e8eba922
Paul Bogdanov <pb180888@gmail.com>
2023-05-03 00:58:59 +0300

    made drycode of Daily Challenge : Go Wildcats of Day2 of Week6

--- Week6/Day2/DailyChallenge_GoWildcats/script.js
+++ Week6/Day2/DailyChallenge_GoWildcats/script.js
@@ -29,38 +29,19 @@ const gameInfo = [
 // The new array should look like this :
 // const usernames = ["john!", "becky!", "susy!", "tyson!"]
 let usernames = [];
-gameInfo.forEach((item) => usernames.push(`${nameUser(item)}!`));
-function nameUser(item) {
-  for (let i in item) {
-    return item[i];
-  }
-}
+gameInfo.forEach((item) => usernames.push(`${item.username}!`));
 console.log(usernames);
 
 // 2. Create an array using forEach that contains the usernames of all players with a score bigger than 5.
 // The new array should look like this : const winners = ["becky", "susy"]
 const winners = [];
 
-gameInfo.forEach((item) => winners.push(nameWinners(item)));
-function nameWinners(item) {
-  for (let i in item) {
-    if (i === "score" && item[i] > 5)
-      // console.log(item.username);
-      return item.username;
-  }
-}
-winners.pop();
-winners.shift();
+gameInfo.forEach((item) => (item.score > 5 ? winners.push(item.username) : ""));
 console.log(winners);
 
 // 3. Find and display the total score of the users. (Hint: The total score is 71)
 const arrScore = [];
-gameInfo.forEach((item) => arrScore.push(score(item)));
-function score(item) {
-  for (let i in item) {
-    if (i === "score") return item[i];
-  }
-}
+gameInfo.forEach((item) => (item.score ? arrScore.push(item.score) : ""));
 let sum = 0;
 arrScore.forEach((item) => (sum = sum + item));
 console.log("The total score of the users is", sum);
