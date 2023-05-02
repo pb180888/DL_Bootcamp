704061c71c34fb514f1eaf9290355c5ee4052a1b
Paul Bogdanov <pb180888@gmail.com>
2023-05-02 19:06:58 +0300

    added Exercise 2 : Ternary Operator of Day2 of Week6

---
+++ Week6/Day2/ExercisesXP/ExerciseXP/Exercise2_TernaryOperator/script.js
@@ -0,0 +1,12 @@
+// Transform the winBattle() function to an arrow function.
+// Create a variable called experiencePoints.
+// Assign to this variable, a ternary operator. If winBattle() is true, the experiencePoints variable should be equal to 10, else the variable should be equal to 1.
+// Console.log the experiencePoints variable.
+
+// function winBattle() {
+//   return true;
+// }
+
+const winBattle = () => true;
+let experience = winBattle() ? (experiencePoints = 10) : (experiencePoints = 1);
+console.log("experiencePoints = ", experience);
