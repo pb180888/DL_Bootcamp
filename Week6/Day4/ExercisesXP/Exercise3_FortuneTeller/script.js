dfcc245827011e02adfc5684919f0391fc6b6c4c
Paul Bogdanov <pb180888@gmail.com>
2023-05-03 21:42:40 +0300

    added style in Exercise 3 : Fortune Teller of Day4 of Week6

--- Week6/Day4/ExercisesXP/Exercise3_FortuneTeller/script.js
+++ Week6/Day4/ExercisesXP/Exercise3_FortuneTeller/script.js
@@ -2,7 +2,9 @@
 // 2. The function should display in the DOM a sentence like "You will be a <job title> in <geographic location>, and married to <partner's name> with <number of children> kids."
 
 (function (numberOfChildren, partnersName, geographicLocation, jobIitle) {
-  document.querySelector(
-    "div"
-  ).innerHTML = `You will be a ${jobIitle} in ${geographicLocation}, and married to ${partnersName} with ${numberOfChildren} kids.`;
+  div = document.querySelector("div");
+  div.style.fontSize = "2rem";
+  div.style.textAlign = "center";
+  div.style.marginTop = "10%";
+  div.innerHTML = `You will be a ${jobIitle} in ${geographicLocation}, and married to ${partnersName} with ${numberOfChildren} kids.`;
 })(2, "Julia", "Czech", "programmer");
