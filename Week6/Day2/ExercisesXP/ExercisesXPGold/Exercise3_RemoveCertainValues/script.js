15f0ae4f67c24959862ba654d8a865478566767d
Paul Bogdanov <pb180888@gmail.com>
2023-05-03 12:17:13 +0300

    added Exercise 3 : Remove Certain Values of ExerciseXP of Day2 of Week6

---
+++ Week6/Day2/ExercisesXP/ExercisesXPGold/Exercise3_RemoveCertainValues/script.js
@@ -0,0 +1,8 @@
+// // 1. Write a JavaScript function to remove: null, 0, "", false, undefined and NaN values from an array.
+// Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
+// Expected result : [15, -22, 47]
+
+const sampleArr = [NaN, 0, 15, false, -22, "", undefined, 47, null, 56, "sdfs"];
+const newArr = [];
+sampleArr.forEach((element) => (Number(element) ? newArr.push(element) : ""));
+console.log(newArr);
