e492eb22542487dece2c3d1474e5799f09527bfd
Paul Bogdanov <pb180888@gmail.com>
2023-05-03 20:22:07 +0300

    added Exercise of Objects of Day3 of Week6

---
+++ Week6/Day3/Objects/Exercise1/script.js
@@ -0,0 +1,10 @@
+// Analyse the code before executing it. What will be the output
+const person = {
+  name: "Lydia",
+  age: 21,
+};
+
+for (const item in person) {
+  console.log(item);
+}
+// consol.log will be name, age
