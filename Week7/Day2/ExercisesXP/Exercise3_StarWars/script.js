cddf0369264e3083f9430c4deb25db70a435222b
Paul Bogdanov <pb180888@gmail.com>
2023-05-08 12:14:51 +0300

    changeg cose in exrcise3 of exercisesXP of Day2 of Week7

--- Week7/Day2/ExercisesXP/Exercise3_StarWars/script.js
+++ Week7/Day2/ExercisesXP/Exercise3_StarWars/script.js
@@ -1,5 +1,5 @@
 // 1. Use the reduce() method to combine all of these into a single string.
 const epic = ["a", "long", "time", "ago", "in a", "galaxy", "far far", "away"];
-const newArr = epic.reduce((acc, item) => acc + item + " ");
+const newArr = epic.reduce((acc, item) => acc + item + " ", " ");
 console.log(newArr);
 // along time ago in a galaxy far far away
