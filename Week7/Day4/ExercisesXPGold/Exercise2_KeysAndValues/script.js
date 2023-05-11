9ed0ed1256e5458c71c4ec494818c8185049502d
Paul Bogdanov <pb180888@gmail.com>
2023-05-11 23:05:49 +0300

    added exercise2 of ExercisesXPGOLD of Day4 of Week7

---
+++ Week7/Day4/ExercisesXPGold/Exercise2_KeysAndValues/script.js
@@ -0,0 +1,16 @@
+// 1. Create a function that takes an object and returns the keys and values as separate arrays.
+// 2. Return the keys sorted alphabetically, and their corresponding values in the same order.
+
+function keysANDvalues(object) {
+  console.log(Object.keys(object), Object.values(object));
+
+  const newArr = Object.entries(object);
+  stickArr = newArr
+    .map((item) => item.reduce((acc, i) => acc + "*" + i))
+    .sort();
+  finallyArr = stickArr.map((item) => item.split("*"));
+  result = Object.fromEntries(finallyArr);
+  console.log(result);
+}
+
+keysANDvalues({ title: "History", author: "Ben Afleck", year: "2005" });
