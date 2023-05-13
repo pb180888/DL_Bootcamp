230e1713e3074ac5d40c65ae6a240b086675f738
Paul Bogdanov <pb180888@gmail.com>
2023-05-13 23:56:38 +0300

    added Mini-Project - Robo-Friends of Day5 of Week7

--- Week7/Day5/MiniProjectRobotFriends/script.js
+++ Week7/Day5/MiniProjectRobotFriends/script.js
@@ -71,46 +71,75 @@ const robots = [
   },
 ];
 
-const keys = Object.values(robots);
-
-console.log(keys);
 const area = document.querySelector(".robots");
 area.style.paddingTop = "150px";
 area.style.display = "flex";
 area.style.flexWrap = "wrap";
 area.style.justifyContent = "center";
-for (let key of keys) {
-  const div = document.createElement("div");
-  div.style.display = "flex";
-  div.style.flexDirection = "column";
-  div.style.alignItems = "center";
-  div.style.justifyContent = "center";
-  div.style.backgroundColor = "rgb(79,193,197)";
-  div.style.backgroundImage = "url(./images/card-pattern.png)";
-  div.style.backgroundSize = "60%";
-  div.style.overflow = "hidden";
-  div.style.margin = "5px";
-  div.style.width = "250px";
-  div.style.height = "340px";
-  div.style.borderRadius = "25px/25px";
-  const divImage = document.createElement("div");
-  divImage.style.width = "220px";
-  divImage.style.height = "220px";
-  divImage.style.backgroundColor = "rgb(48,57,82)";
-  divImage.style.backgroundImage = `url("${key.image}")`;
-  divImage.style.backgroundSize = "cover";
-  divImage.style.backgroundPosition = "center";
-  divImage.style.border = "2px solid";
-  divImage.style.borderRadius = "100%/100%";
-  const divName = document.createElement("div");
-  divName.textContent = `${key.username}`;
-  divName.style.fontFamily = "sans-serif";
-  divName.style.marginTop = "10px";
-  divName.style.fontSize = "1.5em";
-  const divEmail = document.createElement("div");
-  divEmail.textContent = `${key.email}`;
-  div.appendChild(divImage);
-  div.appendChild(divName);
-  div.appendChild(divEmail);
-  area.appendChild(div);
+function displayRobotos(robots) {
+  for (const robot of robots) {
+    const div = document.createElement("div");
+    div.classList.add("robot");
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    div.style.alignItems = "center";
+    div.style.justifyContent = "center";
+    div.style.backgroundColor = "rgb(79,193,197)";
+    div.style.backgroundImage = "url(./images/card-pattern.png)";
+    div.style.backgroundSize = "60%";
+    div.style.overflow = "hidden";
+    div.style.margin = "20px";
+    div.style.width = "250px";
+    div.style.height = "340px";
+    div.style.borderRadius = "25px/25px";
+    const divImage = document.createElement("div");
+    divImage.style.width = "220px";
+    divImage.style.height = "220px";
+    divImage.style.backgroundColor = "rgb(48,57,82)";
+    divImage.style.backgroundImage = `url("${robot.image}")`;
+    divImage.style.backgroundSize = "cover";
+    divImage.style.backgroundPosition = "center";
+    divImage.style.border = "2px solid";
+    divImage.style.borderRadius = "100%/100%";
+    const divName = document.createElement("div");
+    div.classList.add("name");
+    divName.textContent = `${robot.name}`;
+    divName.style.fontFamily = "sans-serif";
+    divName.style.marginTop = "10px";
+    divName.style.fontSize = "1.5em";
+    const divEmail = document.createElement("div");
+    divEmail.textContent = `${robot.email}`;
+    div.appendChild(divImage);
+    div.appendChild(divName);
+    div.appendChild(divEmail);
+    area.appendChild(div);
+  }
 }
+
+const input = document.querySelector("input");
+
+function updateRobotlist(e) {
+  const { value } = e.target;
+  console.log(value);
+  const newList = robots.filter((robot) =>
+    robot.name.toLowerCase().includes(value.toLowerCase())
+  );
+  eraseContainer();
+  displayRobotos(newList);
+}
+
+function eraseContainer() {
+  area.innerHTML = "";
+}
+displayRobotos(robots);
+input.addEventListener("keyup", updateRobotlist);
+// const allRobots = document.querySelectorAll(".robot");
+// allRobots.forEach((item) => {
+//   item.addEventListener("mouseover", function () {
+//     item.style.transition = "all 1s";
+//     item.style.scale = "1.05";
+//   });
+//   item.addEventListener("mouseout", function () {
+//     item.style.scale = "1";
+//   });
+// });
