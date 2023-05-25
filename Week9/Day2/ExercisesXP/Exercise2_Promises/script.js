a461eb97c3fe596b11b20f270f8fb040754fe434
Paul Bogdanov <pb180888@gmail.com>
2023-05-25 21:13:05 +0300

    added Exercise 2 : Promises of ExercisesXP of Day2 of Week9

---
+++ Week9/Day2/ExercisesXP/Exercise2_Promises/script.js
@@ -0,0 +1,14 @@
+// 1. Create a promise that resolves itself in 4 seconds and returns a “success” string.
+// 2. How can you make your promise from part 1 shorter using Promise.resolve() and console.log “success”?
+// 3. Add code to catch errors and console.log ‘Ooops something went wrong’.
+let word = true;
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    word =
+      word === true ? resolve("succes") : reject("Ooops something went wrong");
+  }, 4000);
+});
+promise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+console.log(promise);
