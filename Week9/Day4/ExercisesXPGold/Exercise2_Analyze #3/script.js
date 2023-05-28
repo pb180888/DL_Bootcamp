75fe2a6d149a9524b234c0a929142d2132eee16b
Paul Bogdanov <pb180888@gmail.com>
2023-05-28 17:09:17 +0300

    added Exercise 2: Analyze #3 of ExercisesXPGOLD of Day4 of Week9

---
+++ Week9/Day4/ExercisesXPGold/Exercise2_Analyze #3/script.js
@@ -0,0 +1,37 @@
+let resolveAfter2Seconds = function () {
+  console.log("starting slow promise");
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      resolve("slow");
+      console.log("slow promise is done");
+    }, 2000);
+  });
+};
+
+let resolveAfter1Second = function () {
+  console.log("starting fast promise");
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      resolve("fast");
+      console.log("fast promise is done");
+    }, 1000);
+  });
+};
+
+let concurrentStart = async function () {
+  console.log("==CONCURRENT START with await==");
+  const slow = resolveAfter2Seconds();
+  const fast = resolveAfter1Second();
+  console.log(await slow);
+  console.log(await fast);
+};
+
+setTimeout(concurrentStart, 4000);
+// in 4 sec in consol
+// console.log("==CONCURRENT START with await==");
+// console.log("starting slow promise")
+// console.log("starting fast promise")
+// console.log("fast promise is done")
+// console.log("slow promise is done")
+// slow
+// fast
