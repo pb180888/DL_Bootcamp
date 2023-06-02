8ce8466127390298954f526b9666f5f05dd6d1cc
Paul Bogdanov <pb180888@gmail.com>
2023-06-02 16:41:48 +0300

    added ExercisesXP of Day2 of Week10

---
+++ Week10/Day2/ExercisesXP/ExerciseXP.sql
@@ -0,0 +1,22 @@
+-- Database: public
+
+-- DROP DATABASE IF EXISTS public;
+-- All the items.
+-- select * from items;
+
+-- All the items with a price above 80 (80 not included).
+-- select * from items where item_price > '80';
+
+-- All the items with a price below 300. (300 included)
+-- select * from items where item_price <= '300';
+
+-- All customers whose last name is ‘Smith’ (What will be your outcome?).
+-- select * from customers where last_name = 'Smith';
+-- All customers whose last name is ‘Jones’.
+-- select * from customers where last_name = 'Jones';
+-- All customers whose firstname is not ‘Scott’.
+-- select * from customers where first_name != 'Scott';
+
+
+
+
