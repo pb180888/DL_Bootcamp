978262bb2cd6a6eecabb6777916ffe319657bc50
Paul Bogdanov <pb180888@gmail.com>
2023-06-02 17:57:51 +0300

    added Exercise 1 : Bootcamp of Day2 od Week10

---
+++ Week10/Day2/ExerciseXPGold/Exercise1_Bootcamp.sql
@@ -0,0 +1,13 @@
+-- Database: bootcamp
+
+-- DROP DATABASE IF EXISTS bootcamp;
+-- select * from students;
+-- Fetch the first four students. You have to order the four students alphabetically by last_name.
+-- select * from students order by last_name asc limit 4; 
+
+-- Fetch the details of the youngest student.
+-- select * from students order by birth_date desc limit 1;
+
+
+-- Fetch three students skipping the first two students.
+-- SELECT * FROM students LIMIT 3 OFFSET 2;
