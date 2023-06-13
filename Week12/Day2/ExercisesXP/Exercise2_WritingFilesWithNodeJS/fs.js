a32599ae9ed20d6b1757111ef825a780af7661fb
Paul Bogdanov <pb180888@gmail.com>
2023-06-13 23:14:55 +0300

    added ExercisesXP of Day2 of Week12

---
+++ Week12/Day2/ExercisesXP/Exercise2_WritingFilesWithNodeJS/fs.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+// 1. Create an fs.js file, and use the Node js File System to create a new text file and write to it.
+// fs.appendFile("text.txt", "I love aviation. ", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+// 2. Use the Node js File System to append some other text to the text file. (Example:”Buy orange juice”)
+// fs.appendFile("text.txt", "Cause it's really incredible.", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+// fs.readFile("./text.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString("utf-8"));
+// });
+
+// 3. Use the Node js File System to delete the file.
+// fs.unlink("./text.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
